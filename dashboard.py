0733e356615419ff3c16d208326530ab09f707f3
malfuryj-source <m.alfuryj@gmail.com>
2026-05-29 20:27:14 +0300

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+
+uploaded_file = st.file_uploader(
+"Upload Inventory File",
+type=["xlsx"]
+)
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+
+else:
+        st.info("Please upload an Excel file")
+        st.stop()
+
+df["Stock Value"] = df["Current Stock"] * df["Unit Cost"]
+
+total_stock_value = df["Stock Value"].sum()
+low_stock_count = len(df[df["Current Stock"] < df["Min Stock"]])
+over_stock_count = len(df[df["Current Stock"] > df["Max Stock"]])
+
+st.title("Inventory Dashboard")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric("Stock Value", round(total_stock_value, 2))
+
+with col2:
+    st.metric("Low Stock", low_stock_count)
+
+with col3:
+    st.metric("Over Stock", over_stock_count)
+
+    st.subheader("Inventory Data")
+    st.subheader("Stock Status Distribution")
+
+status_counts = df["Status"].value_counts()
+
+st.bar_chart(status_counts)
+st.dataframe(df)
+st.subheader("Current Stock by Item")
+
+st.bar_chart(
+df.set_index("Item Name")["Current Stock"]
+)
+
